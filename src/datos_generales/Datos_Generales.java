b61fedc7a545e8072539171680aa8a505b14cc99
Venrique <37162608+Venrique@users.noreply.github.com>
2018-03-14 21:52:05 -0600

    new commit

---
+++ src/datos_generales/Datos_Generales.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos_generales;
+
+/**
+ *
+ * @author victor
+ */
+public class Datos_Generales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombres: Victor Enrique");
+        System.out.println("Apellidos: Mendoza Yanes");
+        System.out.println("Edad: 20 años");
+        System.out.println("Nacimiento: 14/02/1998");
+        System.out.println("Música favorita: Dubstep");
+        System.out.println("Graduado Bachiller en: Colegio Salesiano Santa Cecilia");
+        System.out.println("Estudiando Ing. Informática en: Universidad Centroamericana José Simeón Cañas");
+        System.out.println("Elemento favorito de la tabla periódica: Bismuto (Bi)");
+    }
+    
+}
